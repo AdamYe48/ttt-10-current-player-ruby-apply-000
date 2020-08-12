c60178c4969f114b2e9c87a0b35d0283a479d587
Adam Ye <adamye48@gmail.com>
2020-08-12 14:37:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,4 +8,10 @@ def turn_count(board)
   counter
 end
 
-def current_player
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    return "X"
+  elsif turn_count(board) % 2 != 0 
+    return "O"
+  end
+end
